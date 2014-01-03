ae8f7b98770252c6f2f08493246cb49631fb23df
Martin N <mr.avgp@googlemail.com>
2014-01-03 14:27:29 -0500

    As we're loading markers on startup by default, tests need to change.

--- tests/map.js
+++ tests/map.js
@@ -19,36 +19,36 @@ casper.test.begin("Testing markers and popups", 17, function(test) {
   casper.start("https://preview.c9.io/avgp/zueri2go/Zueri2Go/www/index.html?setLng=de");
 
   // Loading the markers manually (as we don't have geolocation available)
-  casper.then(function() {
-    this.evaluate(function() {
-      var pos = {coords: { latitude: 47.376786, longitude: 8.494556 }};
-      loadPanoramicViews(pos, {});
-      loadPlaygrounds(pos, {});
-      loadToilets(pos, {});
-      loadRestaurants(pos, {});
-    });
-  });
+  // casper.then(function() {
+  //   this.evaluate(function() {
+  //     var pos = {coords: { latitude: 47.376786, longitude: 8.494556 }};
+  //     loadPanoramicViews(pos, {}, 2);
+  //     loadPlaygrounds   (pos, {}, 2);
+  //     loadToilets       (pos, {}, 2);
+  //     loadRestaurants   (pos, {}, 2);
+  //   });
+  // });
 
-  testMarkerType(test, "panoramicview.png", 2);
+  testMarkerType(test, "panoramicview.png", 13);
 
-  testMarkerType(test, "playground.png", 27);
+  testMarkerType(test, "playground.png", 28);
   casper.then(function() {
     test.assertTextExists("Altersgruppe:");
     test.assertTextExists("Besonderheiten:");
   });
 
-  testMarkerType(test, "toilets.png", 18);
+  testMarkerType(test, "toilets.png", 40);
   casper.then(function() {
     test.assertTextExists("Gebühr:");
   });
 
-  testMarkerType(test, "restaurant.png", 73);
+  testMarkerType(test, "restaurant.png", 355);
   casper.then(function() {
     test.assertTextExists("Küche:");
   });
 
-  testMarkerType(test, "cafe.png", 17);
-  testMarkerType(test, "fastfood.png", 11);
+  testMarkerType(test, "cafe.png", 86);
+  testMarkerType(test, "fastfood.png", 51);
   casper.then(function() {
     test.assertTextExists("Küche:");
   });
